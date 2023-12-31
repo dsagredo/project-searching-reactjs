4074a431120241d6050bab6bfee5b443d8ef3677
Diego Sagredo <dsagredog@gmail.com>
2023-12-31 11:02:53 -0300

    chore(search): correction update

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
-# Aplicación de React.JS para buscador
+# Proyecto buscador API
 
-![Sin título-1](https://user-images.githubusercontent.com/24228373/195717884-526f5212-fc52-4d4b-b9db-94c64e90eaa6.png)
+<br>
+<div align="center">
+  
+![Badge](https://img.shields.io/badge/-ReactJs-61DAFB?style=for-the-badge&logo=react&logoColor=white)
+![Bootstrap](https://img.shields.io/badge/bootstrap-%238511FA.svg?style=for-the-badge&logo=bootstrap&logoColor=white)
+
+</div>
 
 ## Build-With:
 
-- bootstrap
+-   bootstrap
+-   axios
+-   api rest -> https://pixabay.com
 
 ## Features
 
 ![image](https://user-images.githubusercontent.com/24228373/195712585-33ffb07f-9595-4020-8e81-484c7ef98262.gif)
-
--- src/App.js
+++ src/App.js
@@ -1,44 +1,51 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import Searcher from "./components/Searcher";
-import Results from "./components/Results";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Searcher from './components/Searcher';
+import Results from './components/Results';
 
 const App = () => {
-  const [isInfo, setInfo] = useState('');
-  const [isPag, setPag] = useState(1);
-  const [isTilte, setTitle] = useState();
+    const [isInfo, setInfo] = useState('');
+    const [isPag, setPag] = useState(1);
+    const [isTilte, setTitle] = useState();
 
-  const queryUrl = async (value, page) => {
-    const url = `https://pixabay.com/api/?key=30574607-5270ac4a7557570174723680d&q=${value}&per_page=30&page=${page}`;
-    const {data} = await axios.get(url);
-    setInfo(data.hits);
-  };
+    const queryUrl = async (value, page) => {
+        let q = value;
+        if (value) {
+            q = `&per_page=30&page=${page}&q=${value}`;
+        } else {
+            q = `&per_page=30&page=${page}`;
+        }
 
-  const onSearch = (value) => {
-    queryUrl(value);
-    setTitle(value);
-  };
+        const url = `https://pixabay.com/api/?key=30574607-5270ac4a7557570174723680d${q}`;
+        const { data } = await axios.get(url);
+        setInfo(data.hits);
+    };
 
-  const pagePrev = () => {
-    if(isPag === 1) return null;
-    setPag(isPag - 1);
-  };
+    const onSearch = (value) => {
+        queryUrl(value);
+        setTitle(value);
+    };
 
-  const pageNext = () => setPag(isPag + 1);
+    const pagePrev = () => {
+        if (isPag === 1) return null;
+        setPag(isPag - 1);
+    };
 
-  useEffect(() => {
-    queryUrl(isTilte, isPag);
-  }, [isTilte, isPag]);
+    const pageNext = () => setPag(isPag + 1);
 
-  return (
-    <div className="app container">
-      <div className="jumbotron">
-        <p className="lead text-center">Busscador de Imágenes</p>
-        <Searcher onSearch={onSearch} />
-      </div>
-      <Results isInfo={isInfo} pagePrev={pagePrev} pageNext={pageNext} />
-    </div>
-  );
-}
+    useEffect(() => {
+        queryUrl(isTilte, isPag);
+    }, [isTilte, isPag]);
+
+    return (
+        <div className="app container">
+            <div className="jumbotron">
+                <p className="lead text-center">Busscador de Imágenes</p>
+                <Searcher onSearch={onSearch} />
+            </div>
+            <Results isInfo={isInfo} pagePrev={pagePrev} pageNext={pageNext} />
+        </div>
+    );
+};
 
 export default App;
